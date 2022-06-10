3c00f789e0607538d94c44dc157e2db3264cc6fa
Molly <molly.jones@launchdarkly.com>
2022-06-10 16:30:44 -0700

    update hello.r to add comments, standardize msgs

--- hello.r
+++ hello.r
@@ -1,18 +1,27 @@
 library(reticulate)
 
 # Set SDK_KEY to your LaunchDarkly SDK key
-SDK_KEY="YOUR_SDK_KEY"
+SDK_KEY=""
 # Set FEATURE_FLAG_KEY to the feature flag key you want to evaluate
-FEATURE_FLAG_KEY="YOUR_FEATURE_KEY"
+FEATURE_FLAG_KEY="my-boolean-flag"
 
 ldclient <- import("ldclient")
 
 ldclient$set_config(ldclient$config$Config(SDK_KEY))
 
-user = list(key = "alice@example.com")
+# Set up the user properties. This user should appear on your LaunchDarkly
+# users dashboard soon after you run the demo.
+user = list(key = "example-user-key", name="Sandy")
 
 if (ldclient$get()$variation(FEATURE_FLAG_KEY, user, FALSE)) {
-    print("feature is launched")
+    print("Feature flag %s is true for this user", FEATURE_FLAG_KEY)
 } else {
-    print("feature is hidden")
+    print("Feature flag %s is false for this user", FEATURE_FLAG_KEY)
 }
+
+# Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
+# events to LaunchDarkly before the program exits. If analytics events are not delivered,
+# the user properties and flag usage statistics will not appear on your dashboard. In a
+# normal long-running application, the SDK would continue running and events would be
+# delivered automatically in the background.
+ldclient$get()$close()
