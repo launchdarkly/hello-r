90e79945111d044c9004afeb6f54f94be9f90dc5
Matthew M. Keeler <mkeeler@launchdarkly.com>
2024-06-04 15:30:36 -0400

    chore: Update to latest hello standard format (#6)

--- main.r
+++ main.r
@@ -6,28 +6,58 @@ SDK_KEY <- Sys.getenv("LAUNCHDARKLY_SDK_KEY")
 # Set FEATURE_FLAG_KEY to the feature flag key you want to evaluate
 FEATURE_FLAG_KEY <- Sys.getenv("LAUNCHDARKLY_FLAG_KEY", "sample-feature")
 
+# Set this environment variable to skip the loop process and evaluate the flag
+# a single time.
+CI <- Sys.getenv("CI", "")
+
+show_banner <- function() {
+    cat("\n")
+    cat("        ██       \n")
+    cat("          ██     \n")
+    cat("      ████████   \n")
+    cat("         ███████ \n")
+    cat("██ LAUNCHDARKLY █\n")
+    cat("         ███████ \n")
+    cat("      ████████   \n")
+    cat("          ██     \n")
+    cat("        ██       \n")
+    cat("\n")
+}
+
+show_evaluation_result <- function(key, value) {
+    cat("*** The", key, "feature flag evaluates to", value, "\n")
+
+    if (value) {
+        show_banner()
+    }
+}
+
+flag_change_listener <- function(flag_change) {
+    show_evaluation_result(flag_change$key, flag_change$new_value)
+}
+
 ldclient <- import("ldclient")
+threading <- import("threading")
 
 ldclient$set_config(ldclient$config$Config(SDK_KEY))
 
-if (ldclient$get()$is_initialized()) {
-  cat("SDK successfully initialized!\n")
-} else {
-  cat("SDK failed to initialize\n")
-  exit()
+if (!ldclient$get()$is_initialized()) {
+  cat("*** SDK failed to initialize. Please check your internet connection and SDK credential for any typo.\n")
+  stop()
 }
 
+cat("*** SDK successfully initialized\n")
+
+# Set up the evaluation context. This context should appear on your
+# LaunchDarkly contexts dashboard soon after you run the demo.
 context = ldclient$context$ContextBuilder("example-user-key")$kind("user")$name("Sandy")$build()
 
-if (ldclient$get()$variation(FEATURE_FLAG_KEY, context, FALSE)) {
-    cat("The", FEATURE_FLAG_KEY, "feature flag evaluates to true.\n")
-} else {
-    cat("The", FEATURE_FLAG_KEY, "feature flag evaluates to false\n")
-}
+flag_value = ldclient$get()$variation(FEATURE_FLAG_KEY, context, FALSE)
+show_evaluation_result(FEATURE_FLAG_KEY, flag_value)
 
-# Here we ensure that the SDK shuts down cleanly and has a chance to deliver analytics
-# events to LaunchDarkly before the program exits. If analytics events are not delivered,
-# the user properties and flag usage statistics will not appear on your dashboard. In a
-# normal long-running application, the SDK would continue running and events would be
-# delivered automatically in the background.
-ldclient$get()$close()
+if (CI == "") {
+    change_listener <- ldclient$get()$flag_tracker$add_flag_value_change_listener(
+        FEATURE_FLAG_KEY, context, flag_change_listener)
+
+    threading$Event()$wait()
+}
